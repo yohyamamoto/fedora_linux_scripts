df0350a07f5ed058d6d8f4df6b055d2259d1352d
Yoh Yamamoto <yohyamamoto@gmail.com>
2022-10-28 17:54:08 -0600

    New fedora software installation script

---
+++ softwareinstall.sh
@@ -0,0 +1,2 @@
+sudo dnf update -y
+sudo dnf install -y gfortran scalapack-openmpi-devel openblas-devel lapack-devel vim tig multitail ranger jmol avogadro2 owncloud-client owncloud-client-nautilus htop
