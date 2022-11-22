a4ae23127ade6b442c216c2e3f577222ac9eaec7
Yoh Yamamoto <yohyamamoto@gmail.com>
2022-11-22 13:52:30 -0700

    Update updatezoom.sh

--- updatezoom.sh
+++ updatezoom.sh
@@ -1,3 +1,45 @@
+#!/bin/bash
+
+while getopts xhv name 
+do
+        case $name in
+          #x)xopt=1;;
+          h)hopt=1;;
+          v)vopt=1;;
+          *)echo "Invalid arg";;
+        esac
+done
+
+if [[ ! -z $hopt ]]
+then
+   echo "Usage updatescr [OPTION]..."
+   echo "Update script"
+   echo ""
+   #echo "  -x  use ZZZ for calculations"
+   echo "  -h  display this help and exit"
+   echo "  -v  dispaly version information and exit"
+   echo ""
+   exit
+fi
+
+if [[ ! -z $vopt ]]
+then
+   echo "Feodra update script 0.1"
+   echo "Copyright (C) YY."
+   echo "This is free software: you are free to change and redistribute it."
+   echo "There is NO WARRANTY, to the extent permitted by law."
+   echo ""
+   exit
+fi
+
+
+#shift will flash the command line args
+shift $(($OPTIND -1))
+
+
 echo "This script will update zoom on Fedora/Centos/Redhat"
+read -n 1 -s -r -p "Press any key to continue"
+
 wget https://zoom.us/client/latest/zoom_x86_64.rpm -P /tmp/
 sudo rpm -U /tmp/zoom_x86_64.rpm  
+
