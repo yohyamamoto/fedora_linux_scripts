54e68d3c18499f4e75f281c6147bfa828ea361f0
Yoh Yamamoto <yohyamamoto@gmail.com>
2026-01-05 16:28:29 -0700

    Delete softwareinstall.sh

--- softwareinstall.sh
+++
@@ -1,2 +0,0 @@
-sudo dnf update -y
-sudo dnf install -y gfortran scalapack-openmpi-devel openblas-devel lapack-devel vim tig multitail ranger jmol avogadro2 owncloud-client owncloud-client-nautilus htop
